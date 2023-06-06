1fe0094e96e21eda8837dfae326f04dfd22e355c
yashgrawal007 <135097310+yashgrawal007@users.noreply.github.com>
2023-06-06 13:32:13 +0530

    Create second.java

---
+++ second.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int num1 = 10;
+
+        int num2 = 20;
+
+        int biggestNumber = (num1 > num2) ? num1 : num2;
+
+        System.out.println("The biggest number is: " + biggestNumber);
+
+    }
+
+}
