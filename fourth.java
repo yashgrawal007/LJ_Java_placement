a54ecbc8e40a346d4889fc252424a12dbaa29f9a
yashgrawal007 <135097310+yashgrawal007@users.noreply.github.com>
2023-06-06 13:34:18 +0530

    Create fourth.java

---
+++ fourth.java
@@ -0,0 +1,32 @@
+public class EvenNumbers {
+
+    public static void main(String[] args) {
+
+        for (int i = 2; i <= 20; i += 2) {
+
+            System.out.println(i);
+
+        }
+
+    }
+
+}
+
+--------------------------------
+public class EvenNumbers {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 20; i++) {
+
+            if (i % 2 == 0) {
+
+                System.out.println(i);
+
+            }
+
+        }
+
+    }
+
+}
